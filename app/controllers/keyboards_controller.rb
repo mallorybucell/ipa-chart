4cb4281c1f0ef697be22bf592fc5a586ef3eabf8
Mallory Bucell <m.f.bucell@gmail.com>
2015-04-11 16:11:46 -0400

    working pulmonic hover and sketch of audio play js

--- app/controllers/keyboards_controller.rb
+++
@@ -1,8 +0,0 @@
-class KeyboardsController < ApplicationController
-  def index
-  end
-
-  def vowel_back
-    vowelBackground.png
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'application#index'
 
-  get '/keyboard' => 'keyboards#index'
-  get '/vowelBackground' => 'keyboards#vowel_back'
+  get '/keyboard' => 'application#index'
 end
